8a3eab73109b5c2e2a6a6324de0d2e55fef03490
AbhishekSingh0205 <abhishekzobin02052000@gmail.com>
2022-09-15 15:11:26 +0530

    Starters 56 Done

--- CF Edu 125/D_For_Gamers_By_Gamers.cpp
+++ CF Edu 125/D_For_Gamers_By_Gamers.cpp
@@ -1,13 +1,10 @@
-/*
-    We can calculate minimum length greedily.
-    Now calculating the number of ways using DP.
-*/
+// Not for upsolving.
 #include <bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace std;
 using namespace __gnu_pbds;
-// #define int long long int
+#define int long long int
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 // Instead of less<int>, we can use greater<int>, less_equal<int> for descending, and having multiple occurence respectivly
 template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
@@ -73,99 +70,8 @@ template<class T, class...S>void dbs(string str, T t, S... s) {int idx = str.fin
 #define pra(a,n){}
 #define prm(mat,row,col){}
 #endif
-string s,t;
-int rec(int i,int prevTkn,int j,vector<pii>&a,vector<vvi>&dp){
-    // Base case: if(j==0) that is length is zero then no substring of T should be remaining that from i to a.size() there should be no a[i].ff>prevTkn
-    // if there is so then we will return 0.
-    // else we will return 1
-    // pr(i,prevTkn,j);
-    // pr(a);
-    if(i>=a.size() && j){
-        return 0;
-    }
-    if(j<0){
-        return 0;
-    }
-    if(j==0){
-        for(int k=i;k<a.size();k++){
-            if(a[k].ff>prevTkn){
-                return 0;
-            }
-        }
-        return 1;
-    }
-    if(dp[i][prevTkn][j]!=-1){
-        return dp[i][prevTkn][j];
-    }
-    // basically ith node le skta hu, ya ith ke last wala jaha pe jaa rha hai wha.
-    // Let's say a[j].ff<=a[i].ss then let's I took jth node
-    // then my recursive relation would be 
-    // ans += rec(j+1,a[j].ss,j-1,a,dp) [for each j such that a[j].ff<=a[i].ss]
-    int ans=0;
-    if(a[i].ff<=prevTkn){
-        ans=rec(i+1,prevTkn,j,a,dp);
-        ans%=mod;
-    }
-    else{
-        for(int k=i;k<a.size();k++){
-            if(a[k].ff<=a[i].ss){
-                ans+=rec(k+1,a[k].ss,j-1,a,dp);
-                ans%=mod;
-            }
-        }
-    }
-    return dp[i][prevTkn][j]=ans%mod;
-}
 void solve()
 {
-    cin>>s>>t;
-    int n=s.size(),m=t.size();
-    vector<pii>res;
-    for(int i=0;i<n;i++){
-        if(s[i]==t[0]){
-            bool is=false;
-            for(int j=0;j<m;j++){
-                if(s[i+j]!=t[j]){
-                    is=true;
-                    B;
-                }
-            }
-            if(!is) res.pb({i,i+m-1});
-        }
-    }
-    int cnt=0;
-    int l=res.size();
-    pr(res);
-    int i=0;
-    int prevTkn=-1;
-    // Calculating the minimum length
-    while(i<l){
-        if(prevTkn>=res[i].ff){
-            i++;C;
-        }
-        if(i+1<l){
-            int currL=res[i].ss;
-            while(i+1<l && res[i+1].ff<=currL){
-                i++;
-            }
-            cnt++;
-            prevTkn=res[i].ss;
-            i++;
-            C;
-        }
-        prevTkn=res[i].ss;
-        cnt++;i++;
-    }
-    for(int i=0;i<res.size();i++){
-        res[i].ff++;res[i].ss++;
-    }
-    cout<<cnt<<" ";
-    pr("Worked Here1");
-    vector<vector<vi>>dp(res.size()+1,vvi(502,vi(cnt+1,-1)));
-    pr("W2");
-    int ans=rec(0,0,cnt,res,dp);
-    ans%=mod;
-    cout<<ans<<endl;
 }
 int32_t main()
 {
--- Starters 56/Energetic_Node_Easy_Version.cpp
+++ Starters 56/Energetic_Node_Easy_Version.cpp
@@ -1,13 +1,9 @@
-/*
-    We can calculate minimum length greedily.
-    Now calculating the number of ways using DP.
-*/
 #include <bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace std;
 using namespace __gnu_pbds;
-// #define int long long int
+#define int long long int
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 // Instead of less<int>, we can use greater<int>, less_equal<int> for descending, and having multiple occurence respectivly
 template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
@@ -51,7 +47,7 @@ template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order
 #define NS cout<<"No"<<"\n"
 #define lcm(a,b) (a/__gcd(a,b))*b
 #define pa(a) for(auto e:a)cout<<e<<" "
-const int N = 1e5 + 5;
+const int N = 4e4 + 5;
 int dx[4] = { -1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 int kx[8] = { -1, 1, 0, 0, -1, -1, 1, 1};
@@ -73,105 +69,101 @@ template<class T, class...S>void dbs(string str, T t, S... s) {int idx = str.fin
 #define pra(a,n){}
 #define prm(mat,row,col){}
 #endif
-string s,t;
-int rec(int i,int prevTkn,int j,vector<pii>&a,vector<vvi>&dp){
-    // Base case: if(j==0) that is length is zero then no substring of T should be remaining that from i to a.size() there should be no a[i].ff>prevTkn
-    // if there is so then we will return 0.
-    // else we will return 1
-    // pr(i,prevTkn,j);
-    // pr(a);
-    if(i>=a.size() && j){
-        return 0;
-    }
-    if(j<0){
-        return 0;
-    }
-    if(j==0){
-        for(int k=i;k<a.size();k++){
-            if(a[k].ff>prevTkn){
-                return 0;
-            }
-        }
-        return 1;
-    }
-    if(dp[i][prevTkn][j]!=-1){
-        return dp[i][prevTkn][j];
-    }
-    // basically ith node le skta hu, ya ith ke last wala jaha pe jaa rha hai wha.
-    // Let's say a[j].ff<=a[i].ss then let's I took jth node
-    // then my recursive relation would be 
-    // ans += rec(j+1,a[j].ss,j-1,a,dp) [for each j such that a[j].ff<=a[i].ss]
-    int ans=0;
-    if(a[i].ff<=prevTkn){
-        ans=rec(i+1,prevTkn,j,a,dp);
-        ans%=mod;
-    }
-    else{
-        for(int k=i;k<a.size();k++){
-            if(a[k].ff<=a[i].ss){
-                ans+=rec(k+1,a[k].ss,j-1,a,dp);
-                ans%=mod;
-            }
+vector<int>gr[N];
+int val[N];
+int cst[N];
+int par[N];
+vi m[N];
+void dfs(int src,int pare){
+    par[src]=pare;
+    m[src].pb(val[src]);
+    if(pare>-1){
+        for(auto v:m[pare]){
+            m[src].pb(v);
         }
     }
-    return dp[i][prevTkn][j]=ans%mod;
+    sort(all(m[src]),dsc);
+    for(auto v:gr[src]){
+        if(v!=pare){
+            dfs(v,src);
+        }
+    }
 }
 void solve()
 {
-    cin>>s>>t;
-    int n=s.size(),m=t.size();
-    vector<pii>res;
+    memset(val,0,sizeof val);
+    e1(n);av(a,n);
     for(int i=0;i<n;i++){
-        if(s[i]==t[0]){
-            bool is=false;
-            for(int j=0;j<m;j++){
-                if(s[i+j]!=t[j]){
-                    is=true;
-                    B;
-                }
+        val[i]=a[i];
+    }
+    memset(cst,0,sizeof cst);
+    for(int i=0;i<n-1;i++){
+        e2(x,y);x--;y--;
+        gr[x].pb(y);gr[y].pb(x);
+    }
+
+    for(int i=0;i<n;i++){
+        for(auto v:gr[i]){
+            if(a[v]){
+                val[i]++;
             }
-            if(!is) res.pb({i,i+m-1});
         }
     }
-    int cnt=0;
-    int l=res.size();
-    pr(res);
-    int i=0;
-    int prevTkn=-1;
-    // Calculating the minimum length
-    while(i<l){
-        if(prevTkn>=res[i].ff){
-            i++;C;
-        }
-        if(i+1<l){
-            int currL=res[i].ss;
-            while(i+1<l && res[i+1].ff<=currL){
-                i++;
-            }
-            cnt++;
-            prevTkn=res[i].ss;
-            i++;
+
+    vi prec(N);
+    for(int i=1;i<=N;i++){
+        prec[i-1]=((i*(i+1))/2);
+    }
+    
+    dfs(0,-1);
+    // pra(m,7);
+    
+    e1(q);
+    while(q--){
+        e3(x,t,k);
+        x--;
+        if(t>m[x].size()){
+            cout<<-1<<endl;
             C;
         }
-        prevTkn=res[i].ss;
-        cnt++;i++;
+        int mnm=m[x][t-1];
+        // auto it=m[x].begin();
+        // vi tmp;
+        // for(auto v:m[x]){
+        //     tmp.pb(v);
+        // }
+        // sort(all(tmp),dsc);
+        // if(t>tmp.size()){
+        //     cout<<-1<<endl;C;
+        // }
+        // int mnm=inf;
+        // for(int i=0;i<t;i++){
+        //     mnm=min(mnm,tmp[i]);
+        // }
+        if(mnm==0){
+            cout<<-1<<endl;C;
+        }
+        int rem=k/mnm;
+        auto it=LB(all(prec),rem);
+        int value=*it;
+        if(mnm*value>=k){
+            cout<<it-prec.begin()+1<<endl;
+            C;
+        }
+        if(k%mnm){
+            rem++;
+        }
+        it=LB(all(prec),rem);
+        value=*it;
+        cout<<it-prec.begin()+1<<endl;
+        
     }
-    for(int i=0;i<res.size();i++){
-        res[i].ff++;res[i].ss++;
-    }
-    cout<<cnt<<" ";
-    pr("Worked Here1");
-    vector<vector<vi>>dp(res.size()+1,vvi(502,vi(cnt+1,-1)));
-    pr("W2");
-    int ans=rec(0,0,cnt,res,dp);
-    ans%=mod;
-    cout<<ans<<endl;
 }
 int32_t main()
 {
     ios_base::sync_with_stdio(false);cin.tie(NULL);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     fl(i, 1, t + 1) {
         solve();
     }
