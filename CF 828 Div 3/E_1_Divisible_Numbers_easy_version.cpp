9fe5854863153c76073270ff8c51daa9a1de6899
Abhishek Singh <abhishekzobin02052000@gmail.com>
2022-10-23 07:09:08 +0530

    Added New contests and solving Codechef starters 61

---
+++ CF 828 Div 3/E_1_Divisible_Numbers_easy_version.cpp
@@ -0,0 +1,181 @@
+#include <bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long int
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// Instead of less<int>, we can use greater<int>, less_equal<int> for descending, and having multiple occurence respectivly
+// template<class T> using oset =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+#define pb push_back
+#define mp make_pair
+#define fl(i, a, b) for (int i = a; i < b; i++)
+#define vi vector<int>
+#define e1(a) int a; cin>>a;
+#define e2(a,b) int a,b; cin>>a>>b;
+#define e3(a,b,c) int a,b,c; cin>>a>>b>>c;
+#define av(arr,n) vector<int> arr(n); fl(i,0,n) cin>>arr[i];
+#define aa(arr,n) int arr[n]; fl(i,0,n) cin>>arr[i];
+#define es(s) string(s); cin >> (s);
+#define rz resize
+#define vvi vector<vector<int>>
+#define sz(s) s.size()
+#define mod 1000000007
+#define ff first
+#define ss second
+#define inf 10e15
+#define all(x) (x).begin(), (x).end()
+#define pii pair<int, int>
+#define mii map<int,int>
+#define vl(n) cout << n << "\n"
+#define vs(n) cout << n << " "
+#define el cout<<"\n"
+#define rmin(a,b) (a=min((a),(b)))
+#define rmax(a,b) (a=max((a),(b)))
+#define UB upper_bound
+#define LB lower_bound
+#define vn(n) cout << n
+#define dsc greater<int>()
+#define ps(x,y)  fixed<<setprecision(y)<<x
+#define R return
+#define B break
+#define C continue
+#define YC cout<<"YES"<<"\n"
+#define YS cout<<"Yes"<<"\n"
+#define NC cout<<"NO"<<"\n"
+#define fv(a) for(auto v:(a))
+#define NS cout<<"No"<<"\n"
+#define lcm(a,b) (a/__gcd(a,b))*b
+#define pa(a) for(auto e:a)cout<<e<<" "
+const int N = 1e5 + 5;
+int dx[4] = { -1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+int kx[8] = { -1, 1, 0, 0, -1, -1, 1, 1};
+int ky[8] = {0, 0, -1, 1, -1, 1, -1, 1};
+
+#ifndef ONLINE_JUDGE
+#define pra(a,n)cerr<<#a<<":";for(int i=0;i<n;i++)cerr<<a[i]<<" ";cerr<<endl;
+#define prm(mat,row,col)cerr<<#mat<<":\n";for(int i=0;i<row;i++){for(int j=0;j<col;j++)cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...)dbs(#__VA_ARGS__,__VA_ARGS__)
+template<class S, class T>ostream &operator<<(ostream &os, const pair<S, T> &p) {return os << "(" << p.first << "," << p.second << ")";}
+template<class T>ostream &operator<<(ostream &os, const vector<T> &p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class T>ostream &operator<<(ostream &os, const set<T>&p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class T>ostream &operator<<(ostream &os, const multiset<T>&p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class S, class T>ostream &operator<<(ostream &os, const map<S, T>&p) {os << "["; for (auto&it : p)os << it << " "; return os << "]";}
+template<class T>void dbs(string str, T t) {cerr << str << ":" << t << "\n";}
+template<class T, class...S>void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << ":" << t << ","; dbs(str.substr(idx + 1), s...);}
+#else
+#define pr(...){}
+#define pra(a,n){}
+#define prm(mat,row,col){}
+#endif
+#define MAXN   100001
+ 
+// stores smallest prime factor for every number
+int spf[MAXN];
+ 
+// Calculating SPF (Smallest Prime Factor) for every
+// number till MAXN.
+// Time Complexity : O(nloglogn)
+void sieve()
+{
+    spf[1] = 1;
+    for (int i=2; i<MAXN; i++)
+ 
+        // marking smallest prime factor for every
+        // number to be itself.
+        spf[i] = i;
+ 
+    // separately marking spf for every even
+    // number as 2
+    for (int i=4; i<MAXN; i+=2)
+        spf[i] = 2;
+ 
+    for (int i=3; i*i<MAXN; i++)
+    {
+        // checking if i is prime
+        if (spf[i] == i)
+        {
+            // marking SPF for all numbers divisible by i
+            for (int j=i*i; j<MAXN; j+=i)
+ 
+                // marking spf[j] if it is not
+                // previously marked
+                if (spf[j]==j)
+                    spf[j] = i;
+        }
+    }
+}
+long long binpow(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+void solve()
+{
+    // Kaam krna apna BC kya comment padh rha hai
+    // [(a+1)*(c+1), (b)*(d)] 
+    e2(a,b);
+    e2(c,d);
+    int x1=a,x2=c;
+    mii prm;
+    int y1=b,y2=d;
+    while(spf[a]!=1){
+        prm[spf[a]]++;
+        a/=spf[a];
+    }
+    while(spf[b]!=1){
+        prm[spf[b]]++;
+        b/=spf[b];
+    }
+    pr(prm);
+    for(int i=x1+1;i<=x2;i++){
+        mii prm2;
+        int tmp=i;
+        pr(i);
+        while(spf[tmp]!=1){
+            prm2[spf[tmp]]++;
+            tmp/=spf[tmp];
+        }
+        mii prm3=prm;
+        for(auto &v:prm3){
+            v.ss-=min(prm2[v.ff],v.ss);
+        }
+        int res=1;
+        for(auto v:prm3){
+            res*=binpow(v.ff,v.ss);
+        }
+        int lft=y2/res;
+        int newN=(lft+1)*res;
+        res*=lft;
+        if((res && res<=y2 && res>y1)||(newN && newN>y1 && newN<=y2)){
+            cout<<i<<" ";
+            int oth=res;
+            if(newN && newN>y1 && newN<=y2){
+                cout<<newN<<endl;
+            }
+            else{
+                cout<<res<<endl;
+            }
+            R;
+        }
+    }
+    cout<<-1<<" "<<-1<<endl;
+    
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int t = 1;
+    sieve();
+    cin >> t;
+    fl(i, 1, t + 1) {
+        solve();
+    }
+    return 0;
+}
